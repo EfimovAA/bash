e1ba889a700d7a2b7feed374c50deac7ebbe6868
EfimovAA <myregs.and@gmail.com>
2022-10-23 21:53:15 +0300

    idea: diff vm backup
    test on vmware workstation
    
    planed for proxmox

---
+++ xdelta_backup_vmdk.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+VMS="/mnt/vm"
+TARGET="/mnt/backup/xdelta_vm"
+VM="Ubuntu 22 lts"
+
+function mkshapshot() {
+vmrun -T ws snapshot "$VMS/$VM".vmx bak
+}
+
+function rmshapshot() {
+vmrun -T ws deleteSnapshot "$VMS/$VM".vmx bak
+}
+
+
+function fullbackup() {
+#mkshapshot
+rsync -a "$VMS/$VM".vmdk $TARGET/
+#rmshapshot
+exit
+}
+
+
+function difbackup() {
+#mkshapshot
+xdelta delta -V "$TARGET/$VM".vmdk "$VMS/$VM".vmdk "$VM"_$(date +%Y%m%d).xdelta
+#rmshapshot
+exit
+}
+
+
+[ -f "$TARGET/$VM".vmdk ] && difbackup 
+fullbackup
+
+
+
